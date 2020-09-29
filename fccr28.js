5acfc84630218b643823b87c2c3e70caa6bda128
stefaleon <stefaleon@gmail.com>
2020-09-29 14:03:06 +0300

    28

---
+++ fccr28.js
@@ -0,0 +1,29 @@
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+    // this.handleChange = this.handleChange.bind(this);
+  }
+  // handleChange(event) {
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ input: event.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type='text'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ControlledInput />, document.getElementById('challenge-node'));
